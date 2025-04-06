43a0fb7f496c570102fa61ccf48b8c00ebc88674
Admin <anton.puzderi@gmail.com>
2025-04-06 14:07:54 +0300

    fix linter

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+/* eslint-disable object-curly-spacing */
+
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useState} from 'react';
+import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
+import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
 import cls from './Sidebar.module.scss';
+
+/* eslint-disable object-curly-spacing */
 
 interface SidebarProps {
     className?: string;
@@ -21,12 +24,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <Button
-                data-testid="sidebar-toggle"
-                onClick={onToggle}
-            >
-                toggle
-            </Button>
+            {/* <Button */}
+            {/*    data-testid="sidebar-toggle" */}
+            {/*    onClick={onToggle} */}
+            {/* > */}
+            {/*    toggle */}
+            {/* </Button> */}
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
