128c63e7bd914f36b16afa5bed3bc0c8e63afb0a
Anton102967 <anton.puzderi@gmail.com>
2025-06-14 14:39:48 +0300

    production 5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,60 +1,28 @@
-const path = require('path');
-
 module.exports = {
-    root: true,
-
     env: {
         browser: true,
         es2021: true,
         jest: true,
     },
-
     extends: [
         'plugin:react/recommended',
         'airbnb',
         'plugin:i18next/recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:import/typescript',
     ],
-
     parser: '@typescript-eslint/parser',
-
     parserOptions: {
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-
     plugins: [
         'react',
         '@typescript-eslint',
         'i18next',
         'react-hooks',
-        'import',
     ],
-
-    settings: {
-        'import/resolver': {
-            typescript: {
-                alwaysTryTypes: true,
-            },
-            alias: {
-                map: [
-                    ['entities', path.resolve(__dirname, 'src/entities')],
-                    ['widgets',  path.resolve(__dirname, 'src/widgets')],
-                    ['app', path.resolve(__dirname, 'src/app')],
-                    // добавьте свои алиасы по необходимости
-                ],
-                extensions: ['.ts', '.tsx', '.js', '.jsx'],
-            },
-        },
-    },
-
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -63,26 +31,17 @@ module.exports = {
             2,
             { extensions: ['.js', '.jsx', '.tsx'] },
         ],
-
-        // import-related
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
-        'import/extensions': 'off',
-        'import/no-extraneous-dependencies': 'off',
-
         'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
         'react/function-component-definition': 'off',
         'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'no-param-reassign': 'off',
-        'no-undef': 'off',
-        'react/no-array-index-key': 'off',
-        'arrow-body-style': 'off',
-
-        // i18next
         'i18next/no-literal-string': [
             'error',
             {
@@ -90,30 +49,24 @@ module.exports = {
                 ignoreAttribute: ['data-testid', 'to', 'target'],
             },
         ],
-
-        // length & accessibility
         'max-len': ['error', { ignoreComments: true, code: 130 }],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
-
-        // hooks
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'error',
-
-        // TS-specific
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/ban-ts-comment': 'off',
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
+        'no-param-reassign': 'off',
+        'no-undef': 'off',
+        'react/no-array-index-key': 'off',
+        'arrow-body-style': 'off',
     },
-
     globals: {
-        IS_DEV: true,
-        API: true,
-        PROJECT: true,
+        __IS_DEV__: true,
+        __API__: true,
+        __PROJECT__: true,
     },
-
     overrides: [
         {
-            files: ['/src/**/*.{test,stories}.{ts,tsx}'],
+            files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
             rules: {
                 'i18next/no-literal-string': 'off',
                 'max-len': 'off',
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -5,8 +5,8 @@ import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
 import { useSelector } from 'react-redux';
 import { getSidebarItems } from 'widgets/Sidebar/model/selectors/getSidebarItems';
+import { SidebarItem } from 'widgets/Sidebar/ui/SidebarItem/SidebarItem';
 import cls from './Sidebar.module.scss';
-import { SidebarItem } from '../SidebarItem/SidebarItem';
 
 interface SidebarProps {
     className?: string;
