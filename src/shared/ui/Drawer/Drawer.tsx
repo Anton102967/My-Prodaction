9c75dca75e6b39cd18b8f30e9e09f13ff800394c
Admin <anton.puzderi@gmail.com>
2025-09-22 20:43:48 +0300

    add wseModal hook, upgrade drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,6 +1,7 @@
 import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import React, { memo } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 import { Overlay } from '../../ui/Overlay/Overlay';
 import { Portal } from '../../ui/Portal/Portal';
 import cls from './Drawer.module.scss';
@@ -10,6 +11,7 @@ interface DrawerProps {
     children: React.ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
+    lazy?: boolean;
 }
 
 export const Drawer = memo((props: DrawerProps) => {
@@ -18,13 +20,29 @@ export const Drawer = memo((props: DrawerProps) => {
         children,
         onClose,
         isOpen,
+        lazy,
     } = props;
 
     const { theme } = useTheme();
 
+    const {
+        isClosing,
+        close,
+        isMounted,
+    } = useModal({
+        animationDelay: 300,
+        isOpen,
+        onClose,
+    });
+
     const mods: Mods = {
         [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
     };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
 
     return (
         <Portal>
@@ -35,7 +53,7 @@ export const Drawer = memo((props: DrawerProps) => {
                     'app_drawer',
                 ])}
             >
-                <Overlay onClick={onClose} />
+                <Overlay onClick={close} />
                 <div className={cls.content}>
                     {children}
                 </div>
