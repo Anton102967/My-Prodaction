d940a004773753355ed400fdedcbb1d25282d33d
Admin <anton.puzderi@gmail.com>
2025-10-06 14:45:35 +0300

    add test for isolated component with cypress, part 2

--- cypress/component/EditableProfileCard.cy.tsx
+++ cypress/component/EditableProfileCard.cy.tsx
@@ -1,13 +1,31 @@
 import { EditableProfileCard } from '../../src/features/editableProfileCard';
 import { TestProvider } from '../../src/shared/lib/tests/componentRender/componentRender';
+import { User } from '../../src/entities/User';
 
 describe('EditableProfileCard.cy.tsx', () => {
     it('playground', () => {
-        const apiUrl = Cypress.env('apiUrl');
-        cy.intercept('GET', `${apiUrl}/profile/*`, { fixture: 'profile.json' }).as('getProfile'); cy.mount(
-            <TestProvider>
-                <EditableProfileCard id="1" />
+        const profileId = '1';
+        const user: User = {
+            id: profileId,
+            username: 'testuser',
+        };
+
+        cy.intercept('GET', '**/profile/*', { fixture: 'profile.json' }).as('getProfile');
+
+        cy.mount(
+            <TestProvider
+                options={{
+                    initialState: {
+                        user: { authData: user },
+                    },
+                }}
+            >
+                <EditableProfileCard id={profileId} />
             </TestProvider>,
         );
+        cy.wait('@getProfile');
+        cy.getByTestId('EditableProfileCard.Error').should('not.exist');
+        cy.getByTestId('ProfileCard.firstName').should('have.value', 'test');
+        cy.getByTestId('EditableProfileCardHeader.EditButton').should('exist');
     });
 });
