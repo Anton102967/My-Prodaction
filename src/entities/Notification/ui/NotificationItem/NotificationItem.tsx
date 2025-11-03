8ec9f2ccde468fe2c9fd11459ad2b54707329e20
Admin <anton.puzderi@gmail.com>
2025-11-03 14:21:06 +0300

    redesign navbar (path 1) fix one

--- src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
@@ -1,9 +1,12 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Card, CardTheme } from '@/shared/ui/deprecatted/Card';
 import { Text } from '@/shared/ui/deprecatted/Text';
 import { Notification } from '../../model/types/notification';
 import cls from './NotificationItem.module.scss';
+import { getFeatureFlag } from '@/shared/features';
+import redesignedPopupCls from '@/shared/ui/redesigned/Popups/styles/popup.module.scss';
+import deprecatedPopupCls from '@/shared/ui/deprecatted/Popups/styles/popup.module.scss';
 
 interface NotificationItemProps {
     className?: string;
@@ -12,11 +15,33 @@ interface NotificationItemProps {
 
 export const NotificationItem = memo((props: NotificationItemProps) => {
     const { className, item } = props;
+    const [isActive, setIsActive] = useState(false);
+
+    const activate = useCallback(() => {
+        setIsActive(true);
+    }, []);
+
+    const deactivate = useCallback(() => {
+        setIsActive(false);
+    }, []);
+
+    const activeClass = getFeatureFlag('isAppRedesigned')
+        ? redesignedPopupCls.active
+        : deprecatedPopupCls.active;
 
     const content = (
         <Card
             theme={CardTheme.OUTLINED}
-            className={classNames(cls.NotificationItem, {}, [className])}
+            className={classNames(
+                cls.NotificationItem,
+                { [activeClass]: isActive },
+                [className],
+            )}
+            tabIndex={0}
+            onMouseEnter={activate}
+            onMouseLeave={deactivate}
+            onFocus={activate}
+            onBlur={deactivate}
         >
             <Text title={item.title} text={item.description} />
         </Card>
@@ -25,10 +50,14 @@ export const NotificationItem = memo((props: NotificationItemProps) => {
     if (item.href) {
         return (
             <a
-                className={cls.link}
+                className={classNames(cls.link, { [activeClass]: isActive })}
                 target="_blank"
                 href={item.href}
                 rel="noreferrer"
+                onMouseEnter={activate}
+                onMouseLeave={deactivate}
+                onFocus={activate}
+                onBlur={deactivate}
             >
                 {content}
             </a>
